55f6b2b9ce547c4398984c323dc0d5b041c33624
Dean Gibson <deangibson03@gmail.com>
2024-09-07 16:41:14 +0200

    Updated UI

--- Controllers/DashboardController.cs
+++ Controllers/DashboardController.cs
@@ -6,6 +6,26 @@ namespace PROG6212_MVC.Controllers
     {
         public IActionResult Index()
         {
+            // Hard-coded values for demo purposes
+            var dashboardData = new
+            {
+                PendingClaims = new[]
+                {
+                    new { Id = 1, Description = "Claim 1", Amount = "$500" },
+                    new { Id = 2, Description = "Claim 2", Amount = "$300" }
+                },
+                ApprovedClaims = new[]
+                {
+                    new { Id = 3, Description = "Claim 3", Amount = "$700" },
+                    new { Id = 4, Description = "Claim 4", Amount = "$450" }
+                },
+                RejectedClaims = new[]
+                {
+                    new { Id = 5, Description = "Claim 5", Amount = "$250" }
+                }
+            };
+
+            ViewBag.DashboardData = dashboardData;
             return View();
         }
     }
